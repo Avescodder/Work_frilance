57f6c148a1d02e6823124c08b3aebff8fa6b20eb
seva_kwork <vsevolodmorozov12@gmail.com>
2024-03-20 19:22:33 +0000

    New feature

--- kwork_progrev1.py
+++ kwork_progrev1.py
@@ -152,8 +152,8 @@ async def progrev_func_gaida(update: Update, context: ContextTypes.DEFAULT_TYPE)
     context.job_queue.run_once(sendmesage, time_delta, chat_id=update.effective_user.id, data=progrev_started)
     context.user_data["progrev_started"] = True
 
-
-
+#фича
+    
 async def sendmesage(context: ContextTypes.DEFAULT_TYPE):
     job = context.job
     if job.data:

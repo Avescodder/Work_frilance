750d08891a9be2b2431f26ee0be0b937f625edfb
seva_kwork <vsevolodmorozov12@gmail.com>
2024-05-09 11:29:34 +0100

    elena_chek_chat

--- elena_vol.py
+++ elena_vol.py
@@ -1,48 +1,22 @@
 import requests
-# URL для API Avito
-url = "https://api.avito.ru/messenger/v2/accounts/{I6PdLx7Wy21W2Upg6gvt}/chats"
-
-# Заголовки HTTP-запроса (необходимо указать заголовок Content-Type для JSON)
-headers = {
-    "Content-Type": "application/json",
-    "Authorization URL": "https://avito.ru/oauth",
-    "Authorization": "Bearer lO1R355g_yKz5ki3B0_EVYLYU0BN6omV5i1_alyu",
-    "Token URL": "https://api.avito.ru/token",
-    "messenger":"read",
-    "user":"read"
-}
-
-# JSON-данные для отправки
-def send_data():
-    data = {
-      
-    }
-
-    # Отправка POST-запроса с данными в формате JSON
-    response = requests.post(url, headers=headers, json=data)
-
-    # Проверка статуса ответа
-    if response.status_code == 200:
-        print("Запрос успешно выполнен!")
-        print("Ответ сервера:", response.json())
-    else:
-        print("Ошибка при выполнении запроса:", response.text)
 def get_chats_info():
-    params = {
-        "user_id" : "I6PdLx7Wy21W2Upg6gvt"
+    url = "https://api.avito.ru/messenger/v2/accounts/{user_id}/chats"
+    headers = {
+        "Content-Type": "application/json",
+        "messenger":"read",
+        "Authorization": "2GfAPOiQTNiQ7YQAxyjTZgAAA5uGsXXWZFwa69S3"
+        
     }
 
-# Отправка GET-запроса с параметрами
-    response = requests.get(url, headers=headers, params=params)
 
-   
 
-# Проверка статуса ответа
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Проверка на успешный ответ
         chats_info = response.json()
         return chats_info
-    else:
-        print("Ошибка при получении информации о чатах:", response.text)
+    except requests.HTTPError as e:
+        print("Ошибка при получении информации о чатах:", e)
 
 chats_info = get_chats_info()
 print(chats_info)
